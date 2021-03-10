fd0c384abea67b63a53372ea562540275e670ef1
ls2716 <luki3141@gmail.com>
2021-03-10 18:46:21 +0100

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import BaseMeaning, Person, Word
+from .models import BaseMeaning, Word
 # Register your models here.
-admin.site.register(Person)
 admin.site.register(Word)
 admin.site.register(BaseMeaning)
